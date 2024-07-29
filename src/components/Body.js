55df71b622f74b135fb3699a04b01d04f1d813db
rajes-dev <159983841+rajes-dev@users.noreply.github.com>
2024-07-29 11:23:21 +0530

    tailwind-config

---
+++ src/components/Body.js
@@ -0,0 +1,15 @@
+import Login from "./Login"
+import Browse from './Browse';
+
+
+const Body = () => {
+  return (
+    <>
+     <Login/>
+     <Browse/>
+    </>
+   
+  )
+}
+
+export default Body
